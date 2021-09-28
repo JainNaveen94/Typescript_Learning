bd7b5d9a7ab8c3ea40c4ab39537cb256ff988357
Naveen Jain <naveenjain@Naveens-MacBook-Pro.local>
2021-09-28 20:58:16 +0530

    all core type example

--- BasicTypescriptUnderstanding/Scripts/TypeBasedLogic.ts
+++ BasicTypescriptUnderstanding/Scripts/TypeBasedLogic.ts
@@ -8,6 +8,39 @@ const TypeBasedLogic = {
     // Typescript Code
     tsAdd : function(number1: number, number2: number) {
         return number1 + number2;
+    },
+
+    /** Example to Specify the working of all the Core Types of Typescript */
+    // Typescript Code
+    tsAddAndShowResult: function(n1: number, n2: number, showResult: boolean, resultMessage: string) {
+        const result = n1 + n2;
+        if(showResult) {
+            console.log(resultMessage + result);
+        } else {
+            return result;
+        }
+    },
+    // Demo of Above Function with some useful notes
+    printCustomeTSAddAndShowResult: function() {
+        const num1 = 10.9;
+        /** 
+         * Its Volite the Type Interface as when we initialize the Variable with Value 
+         * then we don't need to explicitly specify the type as due to type interface 
+         * it take value type bydefault.
+         */
+        // const num2: number = 20; // ===>  Wrong Approach
+
+        /**
+         * It is allowed as here we are not initilize the Variable with any value
+         * so we restrict user to use the variable to specific type by explicitly specify it type.
+         */
+        let num2: number;
+        num2 = 20;
+
+        const printResult = true;
+        const resultMessage = "Result : ";
+
+        this.tsAddAndShowResult(num1, num2, printResult, resultMessage);
     }
 }
 
--- BasicTypescriptUnderstanding/index.ts
+++ BasicTypescriptUnderstanding/index.ts
@@ -8,3 +8,7 @@ console.log("TS Code Execution : ", TypeBasedLogic.tsAdd(5, 2.8));
 // When Pass Parameter First as String and another as Number
 console.log("JS Code Execution : ", TypeBasedLogic.jsAdd('5', 2.8));
 // console.log("TS Code Execution : ", TypeBasedLogic.tsAdd('5', 2.8));  // Not allwoed String to be entered here
+
+/** All Core Tyoe Code Comilation */
+console.log("TS Code Execution : ", TypeBasedLogic.tsAddAndShowResult(3.9, 3.6, false, ""));
+TypeBasedLogic.printCustomeTSAddAndShowResult();
